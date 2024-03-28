9252850ee5659573c54c362ada41d236d60a9c9f
Jorge-Hurt <jrh6612@mavs.uta.edu>
2024-03-28 12:02:54 -0500

    Websocket inclusion

--- TWSG_20/src/main/java/uta/cse3310/App.java
+++ TWSG_20/src/main/java/uta/cse3310/App.java
@@ -1,11 +1,128 @@
+// This is example code provided to CSE3310 Fall 2022
+// You are free to use as is, or changed, any of the code provided
+
+// Please comply with the licensing requirements for the
+// open source packages being used.
+
+// This code is based upon, and derived from the this repository
+//            https:/thub.com/TooTallNate/Java-WebSocket/tree/master/src/main/example
+
+// http server include is a GPL licensed package from
+//            http://www.freeutils.net/source/jlhttp/
+
+/*
+ * Copyright (c) 2010-2020 Nathan Rajlich
+ *
+ *  Permission is hereby granted, free of charge, to any person
+ *  obtaining a copy of this software and associated documentation
+ *  files (the "Software"), to deal in the Software without
+ *  restriction, including without limitation the rights to use,
+ *  copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following
+ *  conditions:
+ *
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ *  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ *  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ *  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package uta.cse3310;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
 
-public class App {
+import org.java_websocket.WebSocket;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+import java.time.Instant;
+import java.time.Duration;
 
-  public static void main(String[] args) { 
-    System.out.println("Compilation Success!!!!");
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+public class App extends WebSocketServer {
+
+  
+  private Vector<WordSearchGame> ActiveGames = new Vector<WordSearchGame>();
+
+  private int GameId = 1;
+
+  private int connectionId = 0;
+
+  private Instant startTime;
+
+
+  public App(int port) {
+    
+  }
+
+  public App(InetSocketAddress address) {
+    
+  }
+
+  public App(int port, Draft_6455 draft) {
+    
+  }
+
+  @Override
+  public void onOpen(WebSocket conn, ClientHandshake handshake) {
+
+
+  }
+
+  @Override
+  public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+    
+  }
+
+  @Override
+  public void onMessage(WebSocket conn, String message) {
+    
+  }
+
+  @Override
+  public void onMessage(WebSocket conn, ByteBuffer message) {
+    
+  }
+
+  @Override
+  public void onError(WebSocket conn, Exception ex) {
+    
+  }
+
+  @Override
+  public void onStart() {
+    
+  }
+
+  private String escape(String S) { 
+
+    return "escape";
+    
+  }
+
+  public static void main(String[] args) {
+
+    
 
   }
 }
