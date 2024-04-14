d83314153aeaff63ae61759c1b06f14bb035fc59
Jorge-Hurt <jrh6612@mavs.uta.edu>
2024-04-14 07:28:28 -0500

    updated Player.java

--- html/grid.js
+++ html/grid.js
@@ -4,14 +4,19 @@ function displayPuzzle(puzzleData) {
     const puzzleGrid = puzzleData; 
     const rows = 50;
     const cols = 50;
-    const table = document.createElement("table");
+    const table = document.createElement("table"); 
+    table.style.borderCollapse = "collapse"; 
     for (let i = 0; i < rows; i++) {
         const row = document.createElement("tr");
         for (let j = 0; j < cols; j++) {
             const cell = document.createElement("td");
-            cell.textContent = puzzleGrid[i][j];
+            cell.textContent = puzzleGrid[i][j]; 
+            cell.style.border = "1px solid black"; 
+            cell.style.width = "20px"; 
+            cell.style.height = "20px"; 
+            cell.style.textAlign = "center"; 
             row.appendChild(cell);
-    }
+        }
         table.appendChild(row);
     }
     puzzleContainer.appendChild(table);
@@ -33,7 +38,7 @@ function displayWordList(wordListData) {
     li.style.padding = "5px"; 
     row.appendChild(li);
     count++;
-    if (count === 10) {
+    if (count === 30) {
         ul.appendChild(row);
         row = document.createElement("div");
         row.className = "word-row"; 
--- src/main/java/uta/cse3310/Player.java
+++ src/main/java/uta/cse3310/Player.java
@@ -1,25 +1,32 @@
 package uta.cse3310;
 
-public class Player {
-    private String name; 
-    private int score; 
-
-  public Player(String name, int score) {
-    this.name = name; 
-    this.score = score; 
-  }  
-  public String getName() { 
-    String String = "string";
-    return String; 
-
-  } 
-  public int getScore() { 
-    int score = 0;
-    return score; 
-  } 
-  public void updateScore(int score) {
-    this.score = score;
-  }
-
+public class Player { 
   
-}
+    private String playerUsername; 
+    private int playerIdentifier; 
+    private int score; 
+    private int totalGamesWon; 
+
+    public Player(String playerUsername, int playerIdentifier) {
+      this.playerUsername = playerUsername; 
+      this.playerIdentifier = playerIdentifier; 
+      this.score = 0; 
+      this.totalGamesWon = 0;
+    } 
+    public String getPlayerUsername() {
+      return playerUsername; 
+    } 
+    public int increaseScore(int pointsEarned) {
+     return score + pointsEarned; 
+    } 
+    public int getPlayerScore() {
+      return score; 
+    } 
+    public int increaseWins(int win) {
+      return totalGamesWon + win; 
+    } 
+    public int getPlayerIdentifier() {
+      return playerIdentifier;
+    }
+    
+} 
